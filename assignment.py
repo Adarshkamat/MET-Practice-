ae5de9e70daec7f84be867d3cce513e74a017fb8
Adarshkamat <adarshkumarkamat23@ifheindia.org>
2025-06-12 19:20:02 +0530

    Assign 4

---
+++ assignment.py
@@ -0,0 +1,34 @@
+#1
+# fruits = [["apple","mango"],["orange","pine"],["dragon","grapes"]]
+#print(fruits[-1][0])
+
+#2
+# tu=((0,3),(9,0),(4,3))
+# print(tu[-1][1])
+
+#3
+# stu=[("alice",23),("bob",43)]
+# print(stu[-1][-1])
+
+#4
+# teo=([10,20],[30,40])
+# print(teo[-1][-1])
+
+#5
+# stu=[{"name":"Adarsh","age":19},
+#      {"name":"bhavan","age":19}]
+# print(stu[0]["name"])
+
+#6
+# sub_marks={
+#     "science":[10,40],
+#     "maths":[47,84]
+# }
+# print(sub_marks["science"][1])
+
+#7
+dic={
+    "alice":{"age":32,"id":554},
+     "john":{"age":42,"id":654}
+}
+print(dic["alice"])
--- nested_list.py
+++ nested_list.py
@@ -3,7 +3,6 @@ fruits[0].append("mango")
 print(fruits)
 # a=[len(fruits[0]),len(fruits[1]) )]
 # print(sum(a))
-
 print(fruits[1:2][0][-1])
 
 
