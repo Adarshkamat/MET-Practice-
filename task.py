40543ad9dac75b10891bef713356fc3230985b51
Adarshkamat <adarshkumarkamat23@ifheindia.org>
2025-06-12 18:45:09 +0530

    avgMarks from dict in lists

---
+++ task.py
@@ -0,0 +1,14 @@
+students = [
+    {"name": "Alice", "marks": [80, 85, 90]},
+    {"name": "Bob", "marks": [70, 75, 68]},
+    {"name": "Charlie", "marks": [90, 92, 88]}
+ ]
+# # Task:
+# # Write a function that calculates average marks for each student,
+# # then sorts and prints
+for i in range(0,len(students)):
+    a=(students[i]["marks"])
+    b=sum(a)
+    print(b)
+
+
