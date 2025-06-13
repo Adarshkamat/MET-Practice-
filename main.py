ae24e3d9119df15811975485b4f51729cae2b295
Adarshkamat <adarshkumarkamat23@ifheindia.org>
2025-06-13 18:22:39 +0530

    module2 if-else

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
-print("Started using git")
-print("new")
-print("hello")
+# # Module 2
+# #1 IF
+# if True:
+#     print("bhavan")
+
+# #2 IF-ELSe
+# if True:
+#     print("adarsh")
+# else:
+#     print("Bhavan noob")
+
+# #3 IF-ELIF-ELSe
+# a,b=3,5
+# if a>b:
+#     print(a)
+# elif a<b:
+#     print(b)
+# else:
+#     print("noob")
+
+# #4 IF-ELIF
+
+# if a<b:
+#     print(a)
+# elif b>a:
+#     print(b)
+
+# check if it is a valid combo
+# for every if,else elif block indintation must be at same level
+# at any point, only block will be executed in a given combo
