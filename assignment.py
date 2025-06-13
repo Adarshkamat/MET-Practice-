d545cdf85426c4bffd0aa99abdaf5497d78973e7
Adarshkamat <adarshkumarkamat23@ifheindia.org>
2025-06-13 12:48:34 +0530

    Assign 4

--- assignment.py
+++ assignment.py
@@ -32,3 +32,94 @@ dic={
      "john":{"age":42,"id":654}
 }
 print(dic["alice"])
+
+#8
+billing = [  {
+                  "status":"paid",
+                  "orderNo":4343
+
+        }
+]
+print(billing[0]["status"])
+
+
+#9
+
+tupel= ( {
+          "status":"paid",
+         "orderNo":4343,
+         "amount":4000},
+         {"status":"pending",
+         "orderNo":4343,
+         "amount":7000}
+       )
+print(tupel[0]["amount"])
+
+#10
+lis=[("delhi","36C"),("mumbai","39C")]
+print(lis[-1][-1])
+
+#11
+user={
+    "alice":[65,75,56],
+    "han":[65,57,33],
+    "alex":[76,88,77]
+}
+print(user["alex"][-1])
+
+#12
+lis=[{
+    "product":"oil",
+    "price":5454
+
+},{
+    "product":"milk",
+    "price":54
+
+}
+]
+print(lis[-1]["product"])
+
+
+#13
+
+lis=[("Maths",[65,75,45]),("Science",[76,77,45])]
+print(lis[0][-1][1])
+
+#14
+weeks={
+    "mon":("run","swim")
+    ,"tue":("pray","read")
+    ,"wed":("library","learn")
+    ,"thu":("travel","exercise")
+    ,"fri":("eat","jump")
+    ,"sat":("movie","eat"),
+    "sun":("chill","rest")
+}
+print(weeks["mon"][0])
+
+#15
+students = [
+         {
+        "name": "Alice",
+        "marks": 
+        {
+            
+    "Math": 85,
+            "Science": 90,
+            "English": 78
+        }
+    },
+    {
+        "name": "Bob",
+        "marks":
+          {
+            
+    "Math": 92,
+            "Science": 88,
+            "English": 80
+        }
+    }
+]
+
+print(students[0]["marks"]["Math"])
