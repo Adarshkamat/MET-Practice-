73a15e592328774d7cf06b6715dc23a1c5ed3ec2
Adarshkamat <adarshkumarkamat23@ifheindia.org>
2025-06-15 18:07:47 +0530

    QintupleSum

--- QuintupleSum.py
+++ QuintupleSum.py
@@ -4,18 +4,19 @@ print(b)
 
 found = False
 
-for i in range(0,len(a)):
-    for j in range(i+1,len(a)):
-        k=b-(a[i]+a[j])
-        if k==100:
-           print("indices to remove : ",i,",",j)
-           first,second=max(i,j),min(i,j)
-           a.pop(first)
-           a.pop(second)
-           found=True
-           break
-        if found:
-            break
+for i in range(len(a)):
+    for j in range(len(a)):
+        if i!=j:
+            k=b-(a[i]+a[j])
+            if k==100:
+                print("indices to remove : ",i,",",j)
+                first,second=max(i,j),min(i,j)
+                a.pop(first)
+                a.pop(second)
+                found=True
+                break
+    if found:
+        break
 
 print(a)
 print(sum(a))
