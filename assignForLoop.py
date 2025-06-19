b4e552b577811cf3e729df2bf754f440e49e3711
Adarshkamat <adarshkumarkamat23@ifheindia.org>
2025-06-19 19:07:33 +0530

    Assignment for loop

---
+++ assignForLoop.py
@@ -0,0 +1,61 @@
+# for i in range(1,6):
+#     print(i)
+
+# for i in range(10,21):
+#     if i%2==0:
+#         print(i)
+
+# l=[]
+# for i in range(0,3):
+#     a=input("Enter name:")
+#     l.append(a)
+
+# print(l)
+
+
+# num=[1,2,3,4,5]
+
+# for nu in num:
+#     print(nu**2)
+
+# dic=[{
+#     "name":"adarsh",
+#     "age":32
+# },
+# {
+#     "name":"bahvan",
+#     "age":87
+# }
+# ]
+
+# for i in dic:
+#     print(i.keys())
+#     print(i.values())
+
+
+# fruits=[['apple', 'banana'], ['grape', 'mango']]
+# for i in fruits:
+#     for j in i:
+#         print(j)
+
+# a=input("Enter a String:")
+# for cahr in a:
+#     print(cahr)
+
+# for i in range(5,-1,-1):
+#     print(i)
+
+# dic={}
+# for i in range(0,3):
+#     a=input("enter Sunject")
+#     b=int(input("enter marks"))
+#     dic.update({"name":a,"marks":b})
+#     for key,value in dic.items():
+#       print(f"{key}:{value}")
+
+
+# loop=[("food",65),("food2",97)]
+
+# for i in loop:
+#     print(i[0:2])
+   
