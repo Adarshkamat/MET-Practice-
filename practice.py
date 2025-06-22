e6009e6de4c55d2a9d540c35d78952b352ceb7dc
Adarshkamat <adarshkumarkamat23@ifheindia.org>
2025-06-22 21:00:37 +0530

    d

--- practice.py
+++ practice.py
@@ -33,14 +33,45 @@ def sec_max(lis):
             sec_m=lis[i]
     print(sec_m)
 
-
-sec_max([9,8,3,4,2])
-for i in range(1,111):
+def prime_nos(start,end):
+ for i in range(start,end):
     if prime(i):
         print(f"{i}prime")
     else:
         print(f"{i}Not a prime")
+
+ #Stock BUT AND SELL       
+prices=[7,1,5,3,6,4]
+profit=0
+bestBuy=prices[0]
+for i in range(len(prices)):
+    if(bestBuy<prices[i]):
+        profit=max(profit,prices[i]-bestBuy)
+    bestBuy=min(prices[i],bestBuy)
+
+def power(x,n):
+    binaryForm=n
+    ans=1
+    while binaryForm>0 :
+        if binaryForm%2==1:
+            ans*=x
+        binaryForm//=2
+        x*=x
+    return ans 
+
+
+
+
+
 # print(palindrome("cattac"))
-# print(prime(9))
 # print(fact(7))
+# print(prime(9))
+# sec_max([9,8,3,4,2])
+# prime_nos(1,111)
+# print(profit)
+#  print(power())
+
+
+
+
 
