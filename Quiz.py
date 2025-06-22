9d4a0271599ea34f9fa2f22933a767741d21930a
Adarshkamat <adarshkumarkamat23@ifheindia.org>
2025-06-22 14:01:37 +0530

    Quiz modified

--- Quiz.py
+++ Quiz.py
@@ -29,3 +29,42 @@ for i in questions:
         count+=1
 
 print(f"Your score :{count}/3")  
+
+
+
+
+
+#QUIZ TWO WITH SOME MODIFICATONS
+question=[{
+    "Question":"What is the capital of Telangana",
+    "Option":["A.Hyd","B.Andhra P","C.Khamam"],
+    "Answer":"A"
+},
+{
+    "Question":"What is the capital of India",
+    "Option":["A.Hyd","B.Mumbai ","C.Delhi"],
+    "Answer":"C"
+},{
+    "Question":"How many continents are there",
+    "Option":["A.7","B.5 P","C.3"],
+    "Answer":"A"
+}]
+
+count=0
+history=[]
+for i,q in enumerate(question):
+    print(f"Question{i+1} : ",q["Question"])
+    for j in q["Option"]:
+        print(j)
+    a=input("Enter the answer : ").upper()
+    attempt=(i+1 , a , q["Answer"])
+    history.append(attempt)
+    if a==q["Answer"]:
+       count+=1
+    print()
+    print()
+    
+print(f"Your Score for this Quiz is : {count}/{len(question)}")
+print("Q_no ur_ans  crct_ans")
+for k in history:
+    print("",k)
---
+++ practice.py
@@ -0,0 +1,45 @@
+def palindrome(string):
+    l=len(string)
+ 
+    for i in range(l):
+        if string[i]==string[l-i-1]:
+            pass
+        else:
+            return False
+    return True
+
+def fact(num):
+    f=1
+    for i in range(num+1):
+        if i!=0:
+            f*=i
+    return f
+
+def prime(num):
+    if num < 2:
+        return False
+    for i in range(2,num-1):
+        if num%i==0:
+            return False
+    return True
+
+def sec_max(lis):
+    maxx=max(lis)
+    print(maxx)
+    lenn=len(lis)
+    sec_m=min(lis)
+    for i in range(lenn):
+        if lis[i]!=maxx and lis[i]>sec_m:
+            sec_m=lis[i]
+    print(sec_m)
+
+
+sec_max([9,8,3,4,2])
+for i in range(1,111):
+    if prime(i):
+        print(f"{i}prime")
+    else:
+        print(f"{i}Not a prime")
+# print(palindrome("cattac"))
+# print(prime(9))
+# print(fact(7))
