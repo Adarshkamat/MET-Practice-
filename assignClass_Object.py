4e4717a9d8a1e2ebbebf8b03e6b372628aa78cb8
Adarshkamat <adarshkumarkamat23@ifheindia.org>
2025-06-24 21:29:35 +0530

    Assignment Class and Object

--- assignClass_Object.py
+++ assignClass_Object.py
@@ -77,5 +77,91 @@ print(c.model)
 print(c.speed)
 c.honk()
 
+# Imagine a School System with objects like Student, Teacher, and Classroom. Define suitable
+# properties and methods for each. Create classes and instantiate objects for each, and call their
+# methods.
+class classroom:
+    def __init__(self,class_std,room_no):
+        self.class_std=class_std
+        self.room_no=room_no
+    def room(self):
+        print(f"Teacher teaches students in {self.room_no} room ")
 
-    
+class students:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def study(self):
+        print(f"{self.name} is studying")
+
+class teacher:
+    def __init__(self,name,subject_teaches):
+        self.name=name
+        self.subject=subject_teaches
+    def teches(self):
+        print(f"{self.name} teaches to the stuents")
+
+cl=classroom(8,101)
+cl.room()
+st=students("adarsh",14)
+st.study()
+te=teacher("Dr.Shubhash","science")
+te.teches()
+
+
+
+
+# 4. Consider a Shopping App with objects like Product, Cart, and User. Identify at least 3 attributes
+# and 2 actions for each. Write classes, create objects, and simulate actions like adding products to
+# cart or viewing product details.
+# class shopping_app:
+#     def __init__(self,user):
+#         self.user=user
+#         self.cart=[]
+#     def products(self,product):
+#         a=input("ADD TO CART : ").lower()
+#         if a == "yes":
+#             self.cart.append(product)
+#     def cartii(self):
+#         for i in self.cart:
+#             print(i)
+# shop=shopping_app("Adarsh")
+# shop.products("Chips")
+# shop.products("Soft drink")
+# shop.cartii()
+
+# 5. Identify objects in a Library like Book, Librarian, and Member. Define attributes (like title, author
+# for Book) and actions (like issue, return, read). Write classes, create instances, and call some
+# method
+class book:
+    def __init__(self,title,book_left,author):
+        self.title=title
+        self.book_left=book_left
+        self.author=author
+    def books(self):
+        print(f"""The Title of the book "{self.title}"
+              The author of the book "{self.author}"
+              books left : {self.book_left}
+            """)
+    def book_stock():
+       print("{ self.book_left}")
+b1=book("AgeofFire",10,"Suresh")
+b1.books()
+class librarian:
+    def __init__(self,library,Book,issue_date):
+        self.library=library
+        self.Book=Book
+        self.issue_date=issue_date
+    def book_issued(self):
+        print(f"The issued book is : {self.book}")
+        self.book_left-=1
+
+class member:
+    def __init__(self,user_name,user_id):
+        self.user_name=user_name
+        self.user_id=user_id
+    def mem(self):
+        print(f"The member is {self.user_name} with {self.user_id} id")
+
+user=member("Adarsh","554")
+user.mem()
