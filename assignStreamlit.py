64943d68276432763112aae9898c8793c9860b87
Adarshkamat <adarshkumarkamat23@ifheindia.org>
2025-07-04 19:26:12 +0530

    Streamlit

---
+++ assignStreamlit.py
@@ -0,0 +1,16 @@
+import streamlit as s
+import requests 
+s.title("Come Have Fun")
+word = s.text_input("Enter a word : ")
+btn = s.button(" Click To Generate")
+if btn:
+    url = ""
+    response = requests.get(url)
+    data = response.json()
+    if response.status_code()==200:
+        for item in data :
+            s.write(item["word"])
+    else :
+        s.write("There has been error")
+else:
+    s.toast("Are you Dumb as Bhavan")
