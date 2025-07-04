861862ca496c77ee55fc383438a188d5e1d4a972
Adarshkamat <adarshkumarkamat23@ifheindia.org>
2025-07-04 19:31:34 +0530

    new

--- assignStreamlit.py
+++ assignStreamlit.py
@@ -4,10 +4,10 @@ s.title("Come Have Fun")
 word = s.text_input("Enter a word : ")
 btn = s.button(" Click To Generate")
 if btn:
-    url = "https://api.datamuse.com/words?ml={}"
+    url = "https://api.datamuse.com/words?ml={word}"
     response = requests.get(url)
     data = response.json()
-    if response.status_code()==200:
+    if response.status_code==200:
         for item in data :
             s.write(item["word"])
     else :
