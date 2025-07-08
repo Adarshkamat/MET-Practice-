976da5da01a1f69b6c007abddeed4c732c2a5260
Adarshkamat <adarshkumarkamat23@ifheindia.org>
2025-07-08 19:12:06 +0530

    a

---
+++ 7daySprint.py
@@ -0,0 +1,59 @@
+# 7-7-2025 in kaggle
+# 8-7-2025
+# s="abc12def34gh5"
+# summ=0
+# for char in s :
+#     if char.isdigit():
+#         summ+=int(char) 
+# print(summ) 
+
+
+
+
+
+# s="abc12def34gh5"
+# temp=""
+# summ=0
+# for char in s :
+#     if char.isdigit():
+#         temp+=char
+#     else:
+#         if temp !="":
+#             summ+=int(temp)
+#             temp=""
+# if temp:
+#     summ += int(temp)  
+# print(summ) 
+
+
+# a="apple"
+# b="plead"
+# c=set(a)
+# d=set(b)
+# common=c&d
+# print(common)
+
+
+# a,b,c=10,25,15
+
+# if a>b:
+#     if a>c:
+#         print(f"{a} is greater ")
+# elif b>a:
+#     if b>c:
+#         print(f"{b } is greater ")
+# else:
+#     print(f"{c} is greater ")
+
+
+
+# s="swiss"
+# for ch in s :
+#     if s.count(ch)==1:
+#         print(ch)
+#         break
+
+
+
+
+
